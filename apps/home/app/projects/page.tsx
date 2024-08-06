0d7314461e76d00ece00db0c4e0eff75a1e12c9f
bpasco <benjamin.pasco@hotmail.fr>
2024-08-06 20:12:41 +0200

    home add projects page

---
+++ apps/home/app/projects/page.tsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+  return (
+    <>
+      <div>Arrathon</div>
+      <div>Auth project</div>
+      <div>youtube player</div>
+      <div>zig video encoder</div>
+    </>
+  );
+}
