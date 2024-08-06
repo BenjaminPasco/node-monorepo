ab12a4d6b1cb4228723a1ebb747dc078f10c1809
bpasco <benjamin.pasco@hotmail.fr>
2024-08-06 20:16:45 +0200

    home add some content on home page

--- apps/home/app/page.tsx
+++ apps/home/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <div>finish homepage</div>
+      <div>add db</div>
+      <div>Object storage</div>
+      <div>BiomeJs</div>
+      <div>go cli todos</div>
+      <div>cant use stylex with swc, radix colors</div>
+      <div>cant use @ import shorthand from nextjs with stylex</div>
+      <div>theming in styleX very underwhelming, for now</div>
+    </>
+  );
 }
